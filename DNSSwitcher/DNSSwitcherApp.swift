6581a5f65db1100437df474d3ecaf1b9d0faf364
Cyril Beslay <cicoub13@gmail.com>
2023-04-01 14:46:00 +0200

    Initial Commit

---
+++ DNSSwitcher/DNSSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNSSwitcherApp.swift
+//  DNSSwitcher
+//
+//  Created by Cyril Beslay Home on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DNSSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
